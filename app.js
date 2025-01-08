991f2254bd0cdddff0a4a9d202dbaccb6c997e6d
Simone-Domenici <domenici.simone00@gmail.com>
2025-01-08 15:48:34 +0100

    ADD .gitignore, update package.json with new dependencies, and configure CORS in app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+	})
+)
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
