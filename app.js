8dfba88069fdd8ab47165ab60cffdc230a57eeec
Simone-Domenici <domenici.simone00@gmail.com>
2025-01-08 16:04:43 +0100

    ADD movie router and controller for fetching movies and their reviews

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const notFound = require('./middlewares/notFound');
 const errorsHandler = require('./middlewares/errorsHandler');
+const movieRouter = require('./routers/movieRouter');
 
 app.use(
 	cors({
@@ -15,6 +16,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/movies', movieRouter)
 
 app.use(errorsHandler)
 
