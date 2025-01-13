ad16f0952420ba920d12a915dedee05e2eccf167
Simone-Domenici <domenici.simone00@gmail.com>
2025-01-13 15:32:28 +0100

    ADD JSON parsing middleware and implement review storage functionality in movie controller

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(
 		origin: process.env.CORS_ORIGIN,
 	})
 )
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -36,4 +36,31 @@ function show(req, res) {
 	})
 }
 
-module.exports = { index, show }
+function storeReview(req, res) {
+	const id = req.params.id
+
+	const { text, vote, name } = req.body
+	const intVote = parseInt(vote)
+
+	if (
+		!name ||
+		!intVote ||
+		isNaN(intVote) ||
+		intVote < 1 ||
+		intVote > 5 ||
+		name?.length > 255 ||
+		typeof name !== 'string'
+	) {
+		return res.status(400).json({ message: 'Invalid data' })
+	}
+
+	const sql =
+		'INSERT INTO reviews (text, name, vote, movie_id) VALUES (?, ?, ?, ?)'
+
+	connection.query(sql, [text, name, intVote, id], (err, results) => {
+		if (err) return res.status(500).json({ message: err.message })
+		res.status(201).json({ message: 'Review added', id: results.insertId })
+	})
+}
+
+module.exports = { index, show, storeReview }
